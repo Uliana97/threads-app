d4cc8825108fdd6bc178ff717d955332e1a52f49
Uliana Dmitrieva <uli.dm13@gmail.com>
2023-12-17 23:00:11 +0100

    Onboarding, AccountProfile, zod validation, shadcn

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,35 @@
+import AccauntProfile from "@/components/forms/AccauntProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+  const user = await currentUser();
+
+  const userInfo = {
+    _id: "60f6b8b9e1f9b40004f1b3a6",
+    username: "test",
+    name: "test",
+    bio: "test",
+    image: "https://i.pravatar.cc/150?u=60f6b8b9e1f9b40004f1b3a6",
+  };
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl || "",
+  };
   return (
-    <div>
-      <h1 className="head-class">Onboarding</h1>
-    </div>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threads
+      </p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccauntProfile user={userData} btnTitle="Continue" />
+      </section>
+    </main>
   );
 }
 
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 type TopbarProps = {};
 
@@ -29,6 +30,7 @@ const Topbar: React.FC<TopbarProps> = () => {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
