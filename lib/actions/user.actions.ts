2de6807c7c26190e245b858a61a3c2c3cb445157
Uliana Dmitrieva <uli.dm13@gmail.com>
2023-12-28 01:36:54 +0100

    Search page, UserCard, fetchUsers + filtering and pagination

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface UserUpdateI {
   userId: string;
@@ -86,5 +87,67 @@ export const fetchUserPosts = async (accountId: string) => {
     return threads;
   } catch (err: any) {
     throw new Error(`Failed to fetch user posts: ${err.message}`);
+  }
+};
+
+export const fetchUsers = async ({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) => {
+  try {
+    connectToDB();
+
+    // Calculate the number of users to skip
+    // if we have 45 results and we are on page 1 - skip 0 users
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Create a case-insensitive regular expression for the provided search string
+    const regex = new RegExp(searchString, "i");
+
+    // Create query to filter users.
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId }, // Not include the current user
+    };
+
+    // If the search string is not empty
+    if (searchString.trim() !== "") {
+      query.$or = [
+        // we search for username that starts with the search string
+        { username: { $regex: regex } },
+        // OR we search for name that starts with the search string
+        { name: { $regex: regex } },
+      ];
+    }
+
+    // Define the sort options for the fetched users based on createdAt field and provided sort order
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    // Count the total number of users that match the search criteria (without pagination)
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    // Check if there are more users beyond the current page.
+    // if we have 45 results and we are on page 1 - 45 > 0 + 20
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
   }
 };
