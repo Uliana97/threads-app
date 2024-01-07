7a4069cf8d0cbc496d961371099e8557ef656a01
Uliana Dmitrieva <uli.dm13@gmail.com>
2024-01-07 23:34:22 +0100

    Update Profile header

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 
 interface Props {
@@ -7,7 +8,7 @@ interface Props {
   username: string;
   imgUrl: string;
   bio: string;
-  type?: string;
+  type?: "User" | "Community";
 }
 
 const ProfileHeader = ({
@@ -39,6 +40,20 @@ const ProfileHeader = ({
             <p className="text-base-medium text-gray-1">@{username}</p>
           </div>
         </div>
+        {accountId === authUserId && type !== "Community" && (
+          <Link href="/profile/edit">
+            <div className="flex cursor-pointer gap-3 rounded-lg bg-dark-3 px-4 py-2">
+              <Image
+                src="/assets/edit.svg"
+                alt="logout"
+                width={16}
+                height={16}
+              />
+
+              <p className="text-light-2 max-sm:hidden">Edit</p>
+            </div>
+          </Link>
+        )}
       </div>
 
       <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
