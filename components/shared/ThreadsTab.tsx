e3c51b85f67740eb28311d1dcea6ae0a63faf6a3
Uliana Dmitrieva <uli.dm13@gmail.com>
2024-01-07 21:47:22 +0100

    update ThreadsTab

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 import { fetchUserPosts } from "@/lib/actions/user.actions";
+
 import ThreadCard from "../cards/ThreadCard";
 
 interface Result {
@@ -16,6 +18,11 @@ interface Result {
       image: string;
       id: string;
     };
+    community: {
+      id: string;
+      name: string;
+      image: string;
+    } | null;
     createdAt: string;
     children: {
       author: {
@@ -32,30 +39,40 @@ interface Props {
 }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-  let user: Result = await fetchUserPosts(accountId);
+  let result: Result;
 
-  if (!user) {
+  if (accountType === "Community") {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
+
+  if (!result) {
     redirect("/");
   }
 
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {user.threads.map((thread) => (
+      {result.threads.map((thread) => (
         <ThreadCard
           key={thread._id}
           id={thread._id}
           currentUserId={currentUserId}
           parentId={thread.parentId}
           content={thread.text}
-          // add comment
           author={
             accountType === "User"
-              ? { name: user.name, image: user.image, id: user.id }
+              ? { name: result.name, image: result.image, id: result.id }
               : {
                   name: thread.author.name,
                   image: thread.author.image,
                   id: thread.author.id,
                 }
+          }
+          community={
+            accountType === "Community"
+              ? { name: result.name, id: result.id, image: result.image }
+              : thread.community
           }
           createdAt={thread.createdAt}
           comments={thread.children}
